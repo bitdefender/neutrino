57e1d8a071aa5271f5f29d1ba74a9c909e60c0e5
Alexandra Sandulescu <alecsandra.sandulescu@gmail.com>
2018-05-23 15:20:19 +0300

    configure guest internet connection

--- tools/arm/create_net.sh
+++ tools/arm/create_net.sh
@@ -21,6 +21,8 @@ case "$device" in
         ;;
 esac
 
+sudo ip link del dev $device
+
 # If device doesn't exist add device.
 if ! /sbin/ip link show dev "$device" > /dev/null 2>&1; then
     sudo ip tuntap add mode tap user "$USER" dev "$device"
@@ -34,4 +36,15 @@ sudo /sbin/ip link set dev "$device" up
 
 mkdir -p $PWD/tftp
 
+sudo killall -9 dnsmasq
 sudo dnsmasq --enable-tftp --tftp-root=$PWD/tftp --no-resolv --no-hosts --bind-interfaces --interface $device -F $subnet.2,$subnet.20 -x dnsmasq.pid || true
+
+sudo sysctl net.ipv4.ip_forward=1
+sudo sysctl net.ipv6.conf.default.forwarding=1
+sudo sysctl net.ipv6.conf.all.forwarding=1
+
+interface_name=wlp2s0
+
+sudo iptables -t nat -A POSTROUTING -o $interface_name -j MASQUERADE
+sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i $device -o $interface_name -j ACCEPT
